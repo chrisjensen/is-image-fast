ffbad492ebcbe29ea7d33b26dae29561cda230a3
Chris Jensen <2920476+chrisjensen@users.noreply.github.com>
2020-06-12 13:02:52 +1200

    Fix request to use HEAD method

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const request = require('request-promise-native');
 const urlParse = require('url').parse;
 const isUrl = require('is-url');
 
-module.exports = async (url, accurate, timeout) => {
+module.exports = async (url) => {
   if (!url) return false;
   const http = url.lastIndexOf('http');
   if (http != -1) url = url.substring(http);
@@ -14,11 +14,12 @@ module.exports = async (url, accurate, timeout) => {
   // Remove query string from url
   const last = pathname.search(/[:?&]/);
   if (last != -1) pathname = pathname.substring(0, last);
-  if (/styles/i.test(pathname)) return false;
   try {
-    if (!accurate) return false;
-    if (!timeout) timeout = 60000;
-    const res = await request('GET', url, { timeout });
+    const res = await request({
+      method: 'HEAD',
+      uri: url,
+      resolveWithFullResponse: true,
+    });
     if (!res) return false;
     if (!(res.statusCode >= 200 && res.statusCode < 300)) return false;
     const headers = res.headers;
